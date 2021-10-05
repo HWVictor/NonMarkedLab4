e755a9f401e1cee74813ca154885412a1377ed86
HWVictor <vjv2000@hw.ac.uk>
2021-10-05 11:00:36 +0400

    updated

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const boardElement = document.getElementById("gameboard");
+boardElement.innerHTML = `<div class="col-3 board-card flipped">
+  <div class="face-container">
+    <div class="facedown"></div>
+    <div class="faceup"></div>
+  </div>
+</div>`
